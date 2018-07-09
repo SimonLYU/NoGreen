5bd47ec8743c09a12bcec185ffcf296a81fa3258
吕旭明 <lvxuming@dafycredit.com>
2018-07-09 12:06:29 +0800

    修改胜利机制,步数控制机制

--- NoGreen/NoGreenGame/ViewModel/LYUNoGreenViewModel.m
+++ NoGreen/NoGreenGame/ViewModel/LYUNoGreenViewModel.m
@@ -18,14 +18,14 @@
 - (void)initialize{
     [super initialize];
     self.stage = 1;
-    self.startStep = self.step = 0;
+    self.startStep = self.step = 1;
 }
 
 - (RACCommand *)selectCommand{
     if (!_selectCommand) {
         _selectCommand = [[RACCommand alloc] initWithSignalBlock:^RACSignal *(id input) {
             LYUPixel * pixel = (LYUPixel *)input;
-            ++self.step;
+            --self.step;
             [self selectPixelView:pixel];
             return [RACSignal empty];
         }];
@@ -73,10 +73,17 @@
             pixel.type = kLYUPixelTypeNormal;
         }
     }
+    if ([self detectFail]) {
+        UIAlertController * alertController = [UIAlertController alertControllerWithTitle:@"失败" message:@"步数用尽,请重新开始此关卡!" preferredStyle:UIAlertControllerStyleAlert];
+        [alertController addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            self.step = self.startStep;
+            self.gameMap = self.gameMap;
+        }]];
+        [self.viewController presentViewController:alertController animated:YES completion:nil];
+    }
     if ([self detectWin]) {
-        self.startStep = self.step;
+        self.startStep =  self.step = self.step + ++self.stage;
         [UIUtil showHint:@"恭喜!\n即将进入下一关!" inView:self.viewController.view];
-        ++self.stage;
         [self randomMap];
     }
 }
@@ -84,8 +91,8 @@
 - (RACCommand *)restartCommand{
     if (!_restartCommand) {
         _restartCommand = [[RACCommand alloc] initWithSignalBlock:^RACSignal *(id input) {
-            self.gameMap = self.gameMap;
             self.step = self.startStep;
+            self.gameMap = self.gameMap;
             return [RACSignal empty];
         }];
     }
@@ -99,7 +106,7 @@
             [alertController addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
                 self.stage = 1;
                 [self randomMap];
-                self.startStep = self.step = 0;
+                self.startStep = self.step = 1;
             }]];
             
             [alertController addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil]];
@@ -119,7 +126,23 @@
     self.gameMap = gameMap;
 }
 
+- (BOOL)detectFail{
+    if (self.step <= 0) {//步数为零的时候开始检测场上是否有绿色
+        for (int hang = 0; hang < kNoGreenHeight; ++hang) {
+            for (int lie = 0; lie < kNoGreenLenght; ++lie) {
+                LYUPixel * pixel = self.pixels[hang][lie];
+                if (pixel.type == kLYUPixelTypeSelected) {
+                    return YES;
+                    break;
+                }
+            }
+        }
+    }
+    return NO;
+}
+
 - (BOOL)detectWin{
+
     for (int hang = 0; hang < kNoGreenHeight; ++hang) {
         for (int lie = 0; lie < kNoGreenLenght; ++lie) {
             LYUPixel * pixel = self.pixels[hang][lie];
